7ff0f51be615e574146c5a1a4b9227d3f1778379
Juan Lizondo <j.lizondolancharro@gmail.com>
2024-02-05 13:20:47 +0000

    deployment points scheme

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
 
-# your code here
+# Composición de la página:
+# 1. Analasís exploratorio --> Resumen de los datos, tendencias, estructuras,... más representativas
+# 2. Datos de negocio --> Predicción de los modelos en minutos y distancias del servicio de BiciMad
+# 3. Sistema de recompensas --> Propuesta de mejora de la app desde el punto de vista del usuario
